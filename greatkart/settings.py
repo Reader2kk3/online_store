7cc2a1f46473da18717935776607f968ef401eb3
Reader2kk3 <129747892+Reader2kk3@users.noreply.github.com>
2023-05-24 20:44:30 +0300

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iqa0%k^ws_uj5zhfwp@as^hu=qj9+x9fk_#&0%n1_&d@a)plaq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yamato2kk3@gmail.com'
-EMAIL_HOST_PASSWORD = 'zuaikrrwvyhhrevd'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
